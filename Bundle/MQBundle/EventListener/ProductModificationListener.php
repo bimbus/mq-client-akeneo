a038aee207f85bae07da1e744af514ed32b87a0b
Olivier Lépine <olivier@lepine.fr>
2018-05-03 10:48:01 +0200

    First working version

---
+++ Bundle/MQBundle/EventListener/ProductModificationListener.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Bimbus\Bundle\MQBundle\EventListener;
+
+use Symfony\Component\EventDispatcher\GenericEvent;
+use Pim\Component\Catalog\Model\ProductInterface;
+use Google\Cloud\PubSub\PubSubClient;
+
+class ProductModificationListener
+{
+    private $mqclient;
+
+    private $topic;
+
+    public function __construct(PubSubClient $mqclient, $topic)
+    {
+        $this->mqclient = $mqclient;
+        $this->topic = $topic;
+    }
+
+    public function onPostSave(GenericEvent $event)
+    {
+        $subject = $event->getSubject();
+
+        if (! $subject instanceof ProductInterface) {
+            return;
+        }
+
+        $topic = $this->mqclient->topic($this->topic);
+
+        $topic->publish([
+            'data' => "Updated product",
+            'attributes' => [
+                'sku' => $subject->getIdentifier(),
+            ]
+        ]);
+    }
+}
